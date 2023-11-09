20fd7f3f9375d40b632bf03b48e143640184a3bf
luizandrioli <126569800+luizandrioli@users.noreply.github.com>
2023-11-09 08:50:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function soma_seq(){
      
-    var n=5;
+    var n=parseInt(numero.value);
     var soma=n;
     
     while(n>1){
@@ -11,3 +11,31 @@ function soma_seq(){
     alert(soma);
 }
 
+function fat_seq(){
+    var n=parseInt(camila.value);
+    var fat=n;
+
+    while(n>1){
+        fat=fat*(n-1);
+        n=n-1;
+    }
+    alert(fat)
+}
+
+function potencia(){
+    let n=parseInt(numero3.value);
+    let pote = n;
+
+    if (n<0){
+        alert("numero negativo");
+    }
+    else if(n==0){
+        alert("1");
+    }
+    while (n>1){
+        pote =Math.pow(2,n);
+        n = n - 1;
+        alert(pote);
+    }
+
+}
